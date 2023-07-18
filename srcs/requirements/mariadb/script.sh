bce23e245a5863875f693a4378b0733232adf8c6
wassim elhamdaoui <wassim@waelhamd.waelhamd.42.fr>
2023-07-18 02:22:45 -0500

    feat: add docker compose and create volumes

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -2,7 +2,7 @@
 
 service mariadb start
 
-sed -i "/s/127.0.0.1/0.0.0.0/g" etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/127.0.0.1/0.0.0.0/g" etc/mysql/mariadb.conf.d/50-server.cnf
 
 cat << end > file
 CREATE DATABASE IF NOT EXISTS wordpress;
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -10,9 +10,11 @@ wp core download --allow-root
 chmod -R 0777 wp-content
 chown -R www-data:www-data ../
 
-wp core config --dbname=wordpress --dbuser=waelhamd --dbpass=wassim --dbhost=localhost --allow-root
+wp core config --dbname=wordpress --dbuser=waelhamd --dbpass=wassim --dbhost=mariadb --allow-root
 
 
 mkdir /run/php/
+
+sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g" /etc/php/7.4/fpm/pool.d/www.conf
 
 php-fpm7.4 -F
