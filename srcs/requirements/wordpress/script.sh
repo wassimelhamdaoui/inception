175f534ef254dd4b3837cceee4399060922ade9c
wassim elhamdaoui <wassim@waelhamd.42.fr>
2023-07-19 07:08:38 -0500

    feat: need to add makefile and mariadb volume

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,20 +1,31 @@
 #!/bin/sh.
 
+# sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g" /etc/php/7.4/fpm/pool.d/www.conf
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+# #to use wp insted of php wp-cli.phar
+# chmod +x wp-cli.phar && mv wp-cli.phar /bin/wp
+# wp core download --allow-root
+# sleep 7
+# chmod 777 /var/www/html
+
+# wp core config --dbname=wordpress --dbuser=waelhamd --dbpass=wassim --dbhost=mariadb --allow-root
+
+
+# mkdir /run/php/
+sleep 20
+
+mkdir /run/php
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-#to use wp insted of php wp-cli.phar
-chmod +x wp-cli.phar && mv wp-cli.phar /bin/wp
-
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+cd /var/www/html
+chmod 777 /var/www/html
+if [ ! -f /var/www/html/wp-config.php ]; then
 wp core download --allow-root
-
-chmod -R 0777 wp-content
-chown -R www-data:www-data ../
-
 wp core config --dbname=wordpress --dbuser=waelhamd --dbpass=wassim --dbhost=mariadb --allow-root
-
-
-mkdir /run/php/
-
-sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g" /etc/php/7.4/fpm/pool.d/www.conf
-
+wp core install  --url=waelhamd.42.fr --title=wptitle --admin_user=waelhamd --admin_password=123 --admin_email=wassim.elhamdaoui@uit.ac.ma --allow-root
+wp user create "simple0user" "simpleusermail@gmail.com" --user_pass=userpassword --role=author --allow-root
+fi
 php-fpm7.4 -F
